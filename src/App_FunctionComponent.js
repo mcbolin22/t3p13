52e6b56d5688252b551738ad525285b3756289b7
Colin <mcbolin@hotmail.com>
2024-07-01 15:28:16 +1000

    feat: class vs function basic example

---
+++ src/App_FunctionComponent.js
@@ -0,0 +1,24 @@
+
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const [message, setMessage] = useState("Hello World");
+  const [state2, setState2] = useState("e");
+
+
+  
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+      {/* <h1>{state}{state2}</h1> */}
+      <button onClick={() => setMessage(69)}>
+        Change Message
+      </button>
+    </div>
+  );
+  
+}
+
+export default App;
