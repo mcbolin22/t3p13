6f3581343f76c5f4a0b621314fecbf997f10676a
Colin <mcbolin@hotmail.com>
2024-07-01 16:04:02 +1000

    feat: documented useEffect lifecycle equivalent

--- src/App_FunctionComponent.js
+++ src/App_FunctionComponent.js
@@ -1,11 +1,34 @@
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
 
   const [message, setMessage] = useState("Hello World");
   const [state2, setState2] = useState("e");
+
+  // useEffect with empty dependency array []
+  // depends on nothingm will run when declared
+  // but never again.
+  useEffect(() => {
+    console.log("Component Loaded")
+  }, []);
+
+  // useEffect with return function =
+  // calls the function on componentWillUnmount
+  // but not on anything listed in the dependency array []
+  useEffect(() => {
+    return (() => {
+      console.log("Component will unmount");
+    });
+  }, []);
+
+  // useEffect with variable in dependency array =
+  // depends on variable, will run when declared AND updated
+  // but not on anything not listed in dependancy array
+  useEffect(() => {
+    console.log("Message updated, new value is: " + message)
+  }, [message]);
 
 
   
